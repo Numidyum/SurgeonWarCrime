d71f87d9efa3737b3b19985bb6b9bceef38b5eb9
Numidyum <aliersoyae2003@gmail.com>
2023-12-01 09:06:23 +0300

    Now you can eat organs

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -24,3 +24,47 @@ if (Game.IsMultiplayer and SERVER) or not Game.IsMultiplayer then
     end,1)
 
 end
+
+
+TagsToAdd = {"id_medic", "id_medical", "id_medicaldoctor", "med"}
+
+-- Version and expansion display
+Timer.Wait(function() Timer.Wait(function()
+    local runstring = "\n/// Running NT Surgery Access Fix V "..Version.." ///\n"
+
+    local linelength = string.len(runstring)+4
+    local i = 0
+    while i < linelength do runstring=runstring.."-" i=i+1 end
+
+    print(runstring)
+end,1) end,1)
+
+function upgradeIDCard (instance, ptable)
+    item = instance.item
+    if item.HasTag("jobid:surgeon") then
+        updated = false
+
+		-- Has to be added before to preserve the job identification
+        if not item.HasTag("jobid:medicaldoctor") then
+            item.Tags = "jobid:medicaldoctor," .. item.Tags 
+            updated = true
+        end
+		
+		for i in TagsToAdd do
+			if not item.HasTag(i) then
+				item.AddTag(i)
+				updated = true
+			end
+		end
+
+        if updated and SERVER then
+			Networking.CreateEntityEvent(item, Item.ChangePropertyEventData(item.SerializableProperties[Identifier("Tags")], item))
+        end
+    end
+end
+
+-- jobid:surgeon
+--id_medicaldoctor or jobid:medicaldoctor or id_medic
+Hook.Patch("Barotrauma.Items.Components.IdCard", "OnItemLoaded", upgradeIDCard, Hook.HookMethodType.After)
+
+Hook.Patch("Barotrauma.Items.Components.IdCard", "Initialize", upgradeIDCard, Hook.HookMethodType.After)
